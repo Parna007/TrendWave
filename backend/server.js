b78a8bc3613db86663dbc66634dd47cc6c477279
Parna Das <kathikadad12345@gmail.com>
2024-05-22 21:20:40 +0530

    backend user part 1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
+
 import authRoutes from  './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 
@@ -12,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth' ,authRoutes);
+app.use("/api/users", userRoutes);
+
 
 console.log(process.env.MONGO_URL);
 const PORT = process.env.PORT || 8000;
