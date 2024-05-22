b973f3135a74ff0c6d9f9172c5f952edaacd8d7e
Parna Das <kathikadad12345@gmail.com>
2024-05-22 18:58:44 +0530

    backend code for authentication

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
-import authRoutes from  './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
+import authRoutes from  './routes/auth.route.js';
 import connectMongoDB from "./db/connectMongoDB.js";
+
 
 const app = express();
 dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth' ,authRoutes);
 
